1fe4a4e0bfac83a35660724ef6320dfdf9b742c9
pranita <pranita.kale@neosofttech.com>
2021-12-25 21:03:37 +0530

    initial code push

---
+++ App.js
@@ -0,0 +1,170 @@
+import {
+  ActivityIndicator,
+  Button,
+  Platform,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import {
+  EventType,
+  RNTwilioPhone,
+  twilioPhoneEmitter,
+} from 'react-native-twilio-phone';
+
+import RNCallKeep from 'react-native-callkeep';
+import React from 'react';
+
+const identity = Platform.select({
+  ios: 'Steve',
+  android: 'Larry',
+});
+
+const from = Platform.select({
+  ios: 'client:Steve',
+  android: 'client:Larry',
+});
+
+const callKeepOptions = {
+  ios: {
+    appName: 'TwilioPhone Example',
+    supportsVideo: false,
+  },
+  android: {
+    alertTitle: 'Permissions required',
+    alertDescription: 'This application needs to access your phone accounts',
+    cancelButton: 'Cancel',
+    okButton: 'OK',
+    additionalPermissions: [],
+    // Required to get audio in background when using Android 11
+    foregroundService: {
+      channelId: 'com.example.reactnativetwiliophone',
+      channelName: 'Foreground service for my app',
+      notificationTitle: 'My app is running on background',
+    },
+  },
+};
+
+async function fetchAccessToken() {
+  const response = await fetch(
+    'https://XXXXXX.ngrok.io/accessToken?identity=' +
+      identity +
+      '&os=' +
+      Platform.OS,
+  );
+  const accessToken = await response.text();
+
+  return accessToken;
+}
+
+function App() {
+  const [to, setTo] = React.useState('');
+  const [callInProgress, setCallInProgress] = React.useState(false);
+
+  React.useEffect(() => {
+    return RNTwilioPhone.initialize(callKeepOptions, fetchAccessToken);
+  }, []);
+
+  React.useEffect(() => {
+    const subscriptions = [
+      twilioPhoneEmitter.addListener(EventType.CallConnected, () => {
+        setCallInProgress(true);
+      }),
+      twilioPhoneEmitter.addListener(EventType.CallDisconnected, () => {
+        setCallInProgress(RNTwilioPhone.calls.length > 0);
+      }),
+      twilioPhoneEmitter.addListener(EventType.CallDisconnectedError, data => {
+        console.log(data);
+        setCallInProgress(RNTwilioPhone.calls.length > 0);
+      }),
+    ];
+
+    return () => {
+      subscriptions.map(subscription => {
+        subscription.remove();
+      });
+    };
+  }, []);
+
+  function hangup() {
+    RNCallKeep.endAllCalls();
+  }
+
+  async function call() {
+    if (to === '') {
+      return;
+    }
+
+    setCallInProgress(true);
+
+    try {
+      await RNTwilioPhone.startCall(to, 'My friend', from);
+    } catch (e) {
+      console.log(e);
+      setCallInProgress(false);
+    }
+  }
+
+  async function unregister() {
+    try {
+      await RNTwilioPhone.unregister();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  let content;
+
+  if (callInProgress) {
+    content = (
+      <View>
+        <ActivityIndicator color="#999" style={styles.loader} />
+        <Button title="End call" onPress={hangup} />
+      </View>
+    );
+  } else {
+    content = (
+      <View>
+        <TextInput
+          style={styles.to}
+          onChangeText={text => setTo(text)}
+          value={to}
+          placeholder="Client or phone number"
+          placeholderTextColor="gray"
+        />
+        <Button title="Start call" onPress={call} />
+        <View style={styles.unregister}>
+          <Button title="Unregister" onPress={unregister} />
+        </View>
+      </View>
+    );
+  }
+
+  return <View style={styles.container}>{content}</View>;
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loader: {
+    marginBottom: 40,
+  },
+  to: {
+    height: 50,
+    width: 200,
+    fontSize: 16,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    marginBottom: 40,
+    color: 'gray',
+    textAlign: 'center',
+  },
+  unregister: {
+    marginTop: 40,
+  },
+});
---
+++ android/app/src/main/java/com/customtwiliophone/MainActivity.java
@@ -0,0 +1,28 @@
+package com.customtwiliophone;
+import com.facebook.react.ReactActivity;
+import io.wazo.callkeep.RNCallKeepModule; // Add these import lines
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "customtwiliophone";
+  }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RNCallKeepModule.REQUEST_READ_PHONE_STATE:
+                RNCallKeepModule.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+}
